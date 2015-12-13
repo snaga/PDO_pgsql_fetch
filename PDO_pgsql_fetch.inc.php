214cace36cab51c5a8304b439e4fe47dd9e7c279
Satoshi Nagayasu <snaga@uptime.jp>
2015-12-13 19:42:26 +0900

    Add array handling.

--- PDO_pgsql_fetch.inc.php
+++ PDO_pgsql_fetch.inc.php
@@ -8,11 +8,17 @@ function PDO_pgsql_fetch($stmt)
 
   for ($i = 0; $i < $stmt->columnCount(); $i++) {
     $m = $stmt->getColumnMeta($i);
+    $name = $m['name'];
+
     if ($m['native_type'] === 'json' or $m['native_type'] === 'jsonb') {
-      $m = $stmt->getColumnMeta($i);
-      $name = $m['name'];
       $row2[$i] = json_decode($row[$i], true);
       $row2[$name] = json_decode($row[$i], true);
+    }
+    elseif (substr($m['native_type'], 0, 1) === '_' and substr($row[$i], 0, 1) === '{' and substr($row[$i], -1, 1) === '}')
+    {
+      $v = preg_split('/,/', substr($row[$i], 1, strlen($row[$i])-2));
+      $row2[$i] = $v;
+      $row2[$name] = $v;
     }
   }
   return $row2;
--- pdo_test.php
+++ pdo_test.php
@@ -8,7 +8,10 @@ $user = 'postgres';
 $password = 'postgres';
 
 $pdo = new PDO("pgsql:dbname=$db", $user, $password);
-$pdoStatement = $pdo->query("SELECT 'foo' as foo, row_to_json(pg_user)::jsonb as json FROM pg_user");
+
+print("\n*** json ***\n");
+
+$pdoStatement = $pdo->query("SELECT 'foo' as foo, row_to_json(pg_user)::jsonb as json FROM pg_user WHERE usename = current_user");
 
 #while ($row = $pdoStatement->fetch()) {
 while ($row = PDO_pgsql_fetch($pdoStatement)) {
@@ -17,8 +20,16 @@ while ($row = PDO_pgsql_fetch($pdoStatement)) {
 
   $j = $row['json'];
   var_dump($j);
+}
 
-#  print("usename: " . $j['usename'] . "\n");
+print("\n*** array ***\n");
+
+$pdoStatement = $pdo->query("SELECT array_agg(usename) as foo FROM pg_user");
+
+#while ($row = $pdoStatement->fetch()) {
+while ($row = PDO_pgsql_fetch($pdoStatement)) {
+  $f = $row['foo'];
+  var_dump($f);
 }
 
 ?>
